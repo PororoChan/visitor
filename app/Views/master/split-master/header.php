c4d72f267a84bf4415c059d9890ba1ecbd09948e
RekshoSatriyo <reksho@hyperdatasolusindomandiri.com>
2022-05-20 13:58:57 +0700

    fixed: done

--- app/Views/master/split-master/header.php
+++ app/Views/master/split-master/header.php
@@ -6,7 +6,6 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
     <title>Visitor</title>
 
-    <!-- General CSS Files -->
     <link rel="icon" href="<?= base_url('public/favicon.ico') ?>">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
